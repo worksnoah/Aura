aae52a950df85fdace11af882a34e87c24aa31f4
worksnoah <nlevy2506@gmail.com>
2026-04-07 15:17:23 -0400

    Update lrclib.js

--- lrclib.js
+++ lrclib.js
@@ -1,1 +1,16 @@
+export async function fetchLyrics({ trackName, artistName, albumName, duration }) {
+  const params = new URLSearchParams({
+    track_name: trackName || "",
+    artist_name: artistName || "",
+    album_name: albumName || "",
+    duration: duration ? String(Math.round(duration / 1000)) : ""
+  });
 
+  const response = await fetch(`https://lrclib.net/api/get?${params.toString()}`);
+
+  if (!response.ok) {
+    return null;
+  }
+
+  return response.json();
+}
